7dd8a0d172f7c85fc1d233e552ddcc88ea7d7ed3
Abdelrahman Elzohairy <a.zohairy@gmail.com>
2019-11-08 09:27:40 +0100

    Create main functionality with views and controllers (without doses)

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -8,11 +8,18 @@ class CocktailsController < ApplicationController
   end
 
   def create
-    @cocktail = Cocktail.create(cocktail_params)
-    redirect_to new_cocktail_path
+    @cocktail = Cocktail.new(cocktail_params)
+    @cocktail.save
+    redirect_to cocktail_path(@cocktail)
   end
 
   def new
     @cocktail = Cocktail.new
+  end
+
+  private
+
+  def cocktail_params
+    params.require(:cocktail).permit(:name)
   end
 end
